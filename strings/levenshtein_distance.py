58f0f0f0cf4e7df91f35770ea226481f215e26d5
Paolo Victor <paolovictor@gmail.com>
2011-03-30 12:54:17 -0300

    Added license headers, renamed minimum edit distance to levenshtein distance

--- strings/levenshtein_distance.py
+++ strings/levenshtein_distance.py
@@ -1,6 +1,28 @@
+'''
+Copyright (C) 2011 by Paolo Victor
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+'''
+
 # http://en.wikipedia.org/wiki/Levenshtein_distance
 
-def med(str_a, str_b):
+def levenshtein_distance(str_a, str_b):
     m = [[0 for i in range(len(str_b) + 1)] for k in range(len(str_a) + 1)]
 
     for y in range(1, len(str_a) + 1):
@@ -18,9 +40,10 @@ def med(str_a, str_b):
 
     return m[len(str_a)][len(str_b)]
 
-print med('a brand new option in orange juice', 'abc')
-print med('abc', 'a brand new option in orange juice')
-print med('GACT', 'GCTA')
-print med('ABC', 'A')
-print med('sitting', 'kitten')
-print med('sunday', 'saturday')
+if __name__ == '__main__':
+    print levenshtein_distance('a brand new option in orange juice', 'abc')
+    print levenshtein_distance('abc', 'a brand new option in orange juice')
+    print levenshtein_distance('GACT', 'GCTA')
+    print levenshtein_distance('ABC', 'A')
+    print levenshtein_distance('sitting', 'kitten')
+    print levenshtein_distance('sunday', 'saturday')
